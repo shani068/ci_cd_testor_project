dac9364ad93ae4a6744a3b9ca14b918b12b04400
shani068 <ahtsham6880@gmail.com>
2026-08-25 16:28:28 +0500

    feat: ci yml file

--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
 console.log("Hello from Bun + Node.js!");
 console.log(`Runtime: ${typeof Bun !== "undefined" ? `Bun ${Bun.version}` : `Node ${process.version}`}`);
+const unusedVariable = 'this will fail lint';
+let hello = 'world';
