531331f2597cf70e9ee0d8ba56da2f798ed70a0a
shani068 <ahtsham6880@gmail.com>
2026-08-25 17:02:10 +0500

    feat: bun issue fixed23

--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
 console.log("Hello from Bun + Node.js!");
 console.log(`Runtime: ${typeof Bun !== "undefined" ? `Bun ${Bun.version}` : `Node ${process.version}`}`);
+let hello = 'world';
+let unusedVariable = 'this will fail lint';
